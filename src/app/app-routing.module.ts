69337296c54ad3ba287b635a41c70b86615d8b99
Liena Windyawati <liena1900016101@webmail.uad.ac.id>
2021-06-27 08:35:27 +0700

    pekan 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotComponent } from './folder1/forgot/forgot.component';
+import { LoginComponent } from './folder1/login/login.component';
+import { RegisterComponent } from './folder1/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'login',
+  component:LoginComponent
+},
+{
+  path:'register',
+  component:RegisterComponent
+},
+{
+  path:'forgot',
+  component:ForgotComponent
+},
+{
+  path:'',
+  pathMatch:'full',
+  redirectTo:'/login'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './folder1/login/login.component';
+import { RegisterComponent } from './folder1/register/register.component';
+import { ForgotComponent } from './folder1/forgot/forgot.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesign } from './material/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesign
   ],
   providers: [],
   bootstrap: [AppComponent]
